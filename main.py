b297c631dc898da1d4fd8596ecfd7649c53b79a4
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-03 12:56:34 +0300

    added timezone

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import phonenumbers
 from test import number
-from phonenumbers import geocoder, carrier
+from phonenumbers import geocoder, carrier, timezone
 
 country_num = phonenumbers.parse(number, "CH")
 print(geocoder.description_for_number(country_num, "en"))
 
 service_num = phonenumbers.parse(number, "RO")
 print(carrier.name_for_number(service_num, "en"))
+
+gb_num = phonenumbers.parse(number, "GB")
+print(timezone.time_zones_for_number(gb_num))
