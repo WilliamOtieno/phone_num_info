0457e0dc4aef80c0d30544f2957f970ac4d968ba
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-03 12:47:37 +0300

    added country & ISP

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import phonenumbers
+from test import number
+from phonenumbers import geocoder, carrier
 
+country_num = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_valid_number(country_num, "en"))
+
+service_num = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_num, "en"))
