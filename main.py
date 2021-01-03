37bca14e27b37510931b29896199efcbe5994d1e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-03 12:50:29 +0300

    refactored

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from test import number
 from phonenumbers import geocoder, carrier
 
 country_num = phonenumbers.parse(number, "CH")
-print(geocoder.description_for_valid_number(country_num, "en"))
+print(geocoder.description_for_number(country_num, "en"))
 
 service_num = phonenumbers.parse(number, "RO")
 print(carrier.name_for_number(service_num, "en"))
